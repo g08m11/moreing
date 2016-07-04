9b7d3c55f34cbe05df4a4f9a8c136f2ff4f5e50d
Masaru Gushiken <gushiken@zigexn.co.jp>
2016-07-04 16:42:08 +0900

    add: add to favolite logic

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,15 @@ Rails.application.routes.draw do
   resource :registrations, only: [:new, :create]
   resource :sessions, only: [:new, :create, :destroy]
   resource :settings, only: [:edit, :update]
-  resources :tweets
+  resources :tweets do
+    resource :favorites, only: [:create, :destroy]
+  end
 
   root to: 'registrations#new'
 
-  resources :users, only: [:index, :show]
+  resources :users, only: [:index, :show] do
+    get :favorites, on: :member
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
